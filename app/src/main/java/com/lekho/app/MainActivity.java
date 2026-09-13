cace3723fb6c0ec33cd7b417aac2d0dde1d72112
gneha18119-oss <gneha18119@gmail.com>
2026-09-13 15:21:34 +0530

    Update MainActivity.java

--- app/src/main/java/com/lekho/app/MainActivity.java
+++ app/src/main/java/com/lekho/app/MainActivity.java
@@ -2,23 +2,71 @@ package com.lekho.app;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        TextView textView = new TextView(this);
-        textView.setText("लेखो ऐप\n\nऐप सफलतापूर्वक खुल रही है।");
-        textView.setTextSize(24);
-        textView.setTextColor(Color.BLACK);
-        textView.setBackgroundColor(Color.WHITE);
-        textView.setGravity(17);
+        webView = new WebView(this);
 
-        setContentView(textView);
+        webView.setBackgroundColor(Color.WHITE);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setDatabaseEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAllowContentAccess(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onReceivedError(
+                    WebView view,
+                    WebResourceRequest request,
+                    WebResourceError error) {
+
+                if (request.isForMainFrame()) {
+                    TextView errorText = new TextView(MainActivity.this);
+                    errorText.setText(
+                            "लेखो ऐप खुल रही है,\n\n" +
+                            "लेकिन ऐप की स्क्रीन लोड नहीं हो पाई।\n\n" +
+                            "Error: " + error.getDescription()
+                    );
+                    errorText.setTextSize(18);
+                    errorText.setTextColor(Color.BLACK);
+                    errorText.setBackgroundColor(Color.WHITE);
+                    errorText.setPadding(40, 80, 40, 40);
+
+                    setContentView(errorText);
+                }
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient());
+
+        setContentView(webView);
+
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
