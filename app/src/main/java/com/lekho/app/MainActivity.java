113553f85afa95f931694a2f21a469bad887b45f
gneha18119-oss <gneha18119@gmail.com>
2026-09-14 13:21:49 +0530

    Update MainActivity.java

--- app/src/main/java/com/lekho/app/MainActivity.java
+++ app/src/main/java/com/lekho/app/MainActivity.java
@@ -2,11 +2,7 @@ package com.lekho.app;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.webkit.WebChromeClient;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,28 +14,11 @@ public class MainActivity extends AppCompatActivity {
         WebView webView = new WebView(this);
 
         webView.setBackgroundColor(Color.WHITE);
-
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
-        webView.getSettings().setAllowFileAccess(true);
-        webView.getSettings().setAllowContentAccess(true);
-
-        webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient());
 
         setContentView(webView);
 
-        TextView test = new TextView(this);
-        test.setText(
-                "लेखो ऐप\n\n" +
-                "WebView सफलतापूर्वक चल रहा है।\n\n" +
-                "अभी HTML लोड नहीं की गई है।"
-        );
-        test.setTextSize(22);
-        test.setTextColor(Color.BLACK);
-        test.setGravity(17);
-        test.setBackgroundColor(Color.WHITE);
-
-        setContentView(test);
+        webView.loadUrl("file:///android_asset/index.html");
     }
 }
